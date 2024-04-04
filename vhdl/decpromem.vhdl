c8e8061a10987bb489cf3abe5abb8c4970669028
Mattis Lind <mattis@mattisborgen.se>
2024-04-04 21:09:53 +0200

    Trying to simulate the eeprom as a testbench.

--- vhdl/decpromem.vhdl
+++ vhdl/decpromem.vhdl
@@ -5,23 +5,36 @@ entity decpromem is
 port (
     clk : in std_logic;
     mosi: out std_logic;
-    reset : in std_logic
+    miso: in std_logic;
+    reset : in std_logic;
+    ncs: out std_logic;
+    nhold: out std_logic
 );
 end entity decpromem;
 architecture rtl of decpromem is
-    signal state : unsigned(5 downto 0);
+    signal state : integer range 0 to 33;
+    signal inputShiftReg: std_logic_vector(7 downto 0);
 begin
-    process(clk)
+    process(clk,reset)
     begin
-        if(rising_edge(clk)) then
-            state <= state + 1;
-            if reset = '1' then
-                state <= (others => '0');
+        if reset = '1' then
+            state <= 0;
+            ncs <= '1';
+            nhold <= '1';
+        elsif (falling_edge(clk)) then
+            if (state < 33) then
+                state <= state + 1; 
             end if;
-            if (state = 6 or state = 7) then
-              mosi <= '1';
+            if (state = 7 or state = 8) then
+                mosi <= '1';
             else 
-              mosi <= '0';
+                mosi <= '0';
+            end if;
+            if (state = 1) then 
+                ncs <= '0';
+            end if;
+            if (state > 16 and state < 24) then
+                inputShiftReg <= inputShiftReg(inputShiftReg'high - 1 downto inputShiftReg'low) & miso;           
             end if;
         end if;
     end process;
---
+++ vhdl/testbench.vhd
@@ -0,0 +1,109 @@
+-- Testbench for the Alfaskop 3550 system
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.textio.all;
+ 
+entity testbench is
+-- empty
+end testbench; 
+
+architecture tb of testbench is
+-- DUT component
+
+component decpromem is
+port (
+    clk : in std_logic;
+    mosi: out std_logic;
+    miso: in std_logic;
+    reset : in std_logic;
+    ncs: out std_logic;
+    nhold: out std_logic
+);
+end component;
+type stateType is (STORECOMMAND, STOREADDRESS, OUTPUTDATA);
+signal state: stateType;
+signal clkInput, reset : std_logic;
+signal cmd : std_logic_vector (7 downto 0);
+signal ncs: std_logic;
+signal mosi: std_logic;
+signal miso: std_logic;
+signal commandCount: integer range 0 to 7;
+signal addressCount: integer range 0 to 16;
+signal commandRegister: std_logic_vector (7 downto 0);
+signal addressRegister: std_logic_vector (15 downto 0);
+signal dataCount: integer range 0 to 7;
+signal dataOut: std_logic_vector (7 downto 0);
+begin
+
+  -- Connect DUT
+  DUT: decpromem port map(
+-- Clk is to be generated externally    
+    clk => clkInput,
+    reset => reset,
+    ncs => ncs,
+    mosi => mosi,
+    miso => miso
+    );
+  CLK: process
+  begin
+    reset <= '0';
+    wait for 200 ns;
+    reset <= '1';
+    wait for 200 ns;
+    reset <= '0';
+    wait for 200 ns;
+    for i in 1 to 64 loop
+      clkInput <= '0'; 
+      wait for 200 ns;
+      clkInput <= '1';
+      wait for 200 ns;  
+    end loop;
+    assert false report "Test done." severity note;
+    wait;
+  end process;
+
+  EEPROM: process(clkInput, ncs)
+  begin
+    if ncs = '1' then
+      state <= STORECOMMAND;
+      addressCount <= 0;
+      commandCount <= 0;
+      dataOut <= "01010101";
+      commandRegister <= "00000000";
+      addressRegister <= "0000000000000000";
+    elsif (rising_edge(clkInput)) then
+      case state is
+        when STORECOMMAND =>
+          commandRegister <= commandRegister(commandRegister'high - 1 downto commandRegister'low) & mosi; 
+          if commandCount = 7 then
+            state <= STOREADDRESS;
+          else 
+            commandCount <= commandCount + 1;
+          end if;
+        when STOREADDRESS =>
+          addressRegister <= addressRegister(addressRegister'high - 1 downto addressRegister'low) & mosi; 
+          if addressCount = 15 then
+            state <= OUTPUTDATA;
+          else
+            addressCount <= addressCount + 1;
+          end if;
+        when OUTPUTDATA =>
+      end case;
+    elsif (falling_edge(clkInput)) then
+      case state is
+        when OUTPUTDATA =>
+          dataOut <= dataOut(6 downto 0) & "0"; 
+          miso <= dataOut(dataOut'high);
+          if dataCount = 7 then 
+            dataCount <= 0;
+          else 
+            dataCount <= dataCount + 1;
+          end if;
+        when STOREADDRESS =>
+        when STORECOMMAND =>
+      end case;
+    end if;
+  end process;
+
+end tb;
