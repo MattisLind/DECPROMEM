3fb00eb23d498e9ed9b49cc5c57f346b1da450f6
Mattis Lind <mattis@iotcomms.io>
2025-07-30 17:19:19 +0200

    Updated JED file after fixing pinfile logic in the workflow.

--- vhdl/ATF1508.vhdl
+++ vhdl/ATF1508.vhdl
@@ -44,69 +44,69 @@ port (
     asl: out std_logic;
     mdenl: out std_logic;
     sdenl: out std_logic
---PIN:CHIP "src/ATF1508" ASSIGNED TO AN PLCC84
---PIN:miso    : 73
---PIN:spiclk  : 76
---PIN:ncs     : 74
---PIN:mosi    : 75
---PIN:nhold   : 77
---PIN:bdcokh  : 84
---PIN:bdsl    : 12
---PIN:bwritel : 11
---PIN:brplyl  : 10
---PIN:moe     :  8
---PIN:data_0  :  5
---PIN:data_5  :  4
---PIN:mle     : 22
---PIN:data_7  : 21
---PIN:data_6  : 20
---PIN:data_3  : 18
---PIN:data_4  : 17
---PIN:data_2  : 16
---PIN:data_1  : 15
---PIN:TDI     : 14
---PIN:ioa_3   : 31
---PIN:ioa_2   : 30
---PIN:ioa_0   : 29
---PIN:ioa_1   : 28
---PIN:ioa_4   : 27
---PIN:busdir  : 25
---PIN:TMS     : 23
---PIN:ioa_5   : 41
---PIN:msiz    : 40
---PIN:bwlbl   : 39
---PIN:A_6     : 37
---PIN:A_5     : 36
---PIN:nmcehigh: 44
---PIN:mcelow  : 45
---PIN:ma_0    : 48
---PIN:ma_5    : 35
---PIN:ma_3    : 33
---PIN:ma_1    : 34
---PIN:ma_2    : 50
---PIN:ma_6    : 52
---PIN:ma_4    : 51
---PIN:A_4     : 63
---PIN:A_3     : 9
---PIN:bsdenl  : 55
---PIN:A_2     : 54
---PIN:mwe     : 46
---PIN:busoe   : 24
---PIN:mhe     : 49
---PIN:TCK     : 62
---PIN:A_0     : 6
---PIN:A_1     : 57
---PIN:bwhbl   : 64
---PIN:bmdenl  : 56
---PIN:basl    : 65
---PIN:biosel  : 67
---PIN:TDO     : 71
---PIN:bssxl   : 68
---PIN:CLK     : 83
---PIN:binitl  : 1
---PIN:asl     : 58
---PIN:mdenl   : 60
---PIN:sdenl   : 61    
+--PIN: CHIP "ATF1508" ASSIGNED TO AN PLCC84
+--PIN: miso : 73
+--PIN: spiclk : 76
+--PIN: ncs : 74
+--PIN: mosi : 75
+--PIN: nhold : 77
+--PIN: bdcokh : 84
+--PIN: bdsl : 12
+--PIN: bwritel : 11
+--PIN: brplyl : 10
+--PIN: moe : 8
+--PIN: data_0 : 5
+--PIN: data_5 : 4
+--PIN: mle : 22
+--PIN: data_7 : 21
+--PIN: data_6 : 20
+--PIN: data_3 : 18
+--PIN: data_4 : 17
+--PIN: data_2 : 16
+--PIN: data_1 : 15
+--PIN: TDI : 14
+--PIN: ioa_3 : 31
+--PIN: ioa_2 : 30
+--PIN: ioa_0 : 29
+--PIN: ioa_1 : 28
+--PIN: ioa_4 : 27
+--PIN: busdir : 25
+--PIN: TMS : 23
+--PIN: ioa_5 : 41
+--PIN: msiz : 40
+--PIN: bwlbl : 39
+--PIN: A_6 : 37
+--PIN: A_5 : 36
+--PIN: nmcehigh : 44
+--PIN: mcelow : 45
+--PIN: ma_0 : 48
+--PIN: ma_5 : 35
+--PIN: ma_3 : 33
+--PIN: ma_1 : 34
+--PIN: ma_2 : 50
+--PIN: ma_6 : 52
+--PIN: ma_4 : 51
+--PIN: A_4 : 63
+--PIN: A_3 : 9
+--PIN: bsdenl : 55
+--PIN: A_2 : 54
+--PIN: mwe : 46
+--PIN: busoe : 24
+--PIN: mhe : 49
+--PIN: TCK : 62
+--PIN: A_0 : 6
+--PIN: A_1 : 57
+--PIN: bwhbl : 64
+--PIN: bmdenl : 56
+--PIN: basl : 65
+--PIN: biosel : 67
+--PIN: TDO : 71
+--PIN: bssxl : 68
+--PIN: CLK : 83
+--PIN: binitl : 1
+--PIN: asl : 58
+--PIN: mdenl : 60
+--PIN: sdenl : 61    
 );
 end entity ATF1508;
 architecture rtl of ATF1508 is
